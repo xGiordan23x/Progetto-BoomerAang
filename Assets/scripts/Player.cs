cbe6dac480ed538856a4b8fb7269821ad2ea429d
Giordano23 <francigio23@gmail.com>
2023-04-18 17:58:09 +0200

    movimento player e prove parabola

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public BezierCurve curva;
+    [Header("Curve")]
+    public BezierCurve curve;
+    public BezierPoint[] points;
     public Vector2 lastDirection;
     public float speed;
-    
+    public bool isReturning;
+
+   
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        if (!isReturning)
+        {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-        transform.Translate(new Vector3(horizontal, vertical, 0) * speed);
+            Vector3 movement = new Vector3(horizontal, vertical, 0);
+            if(movement != Vector3.zero) 
+            {
+                Move(movement);
+            }
+        }
+        else
+        {
+            curve[1].transform.position = transform.position;
+           
+        }
+        
+    }
+
+    public void Move(Vector3 movement)
+    {
+        transform.Translate(movement.normalized * speed * Time.deltaTime);
+        lastDirection = movement;
     }
 }
