86552b74819889cf5cfd37d6e74b6e786e429992
Giordano23 <francigio23@gmail.com>
2023-04-20 15:27:31 +0200

    modifiche script player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -10,8 +8,16 @@ public class Player : MonoBehaviour
     public Vector2 lastDirection;
     public float speed;
     public bool isReturning;
+    private bool isFacingRight;
+    private bool isFacingLeft;
+    private bool isFacingUp;
+    private bool isFacingDown;
 
-   
+
+    private void Start()
+    {
+        points = curve.GetAnchorPoints();
+    }
     private void Update()
     {
         if (!isReturning)
@@ -20,7 +26,7 @@ public class Player : MonoBehaviour
             float vertical = Input.GetAxisRaw("Vertical");
 
             Vector3 movement = new Vector3(horizontal, vertical, 0);
-            if(movement != Vector3.zero) 
+            if (movement != Vector3.zero)
             {
                 Move(movement);
             }
@@ -28,14 +34,38 @@ public class Player : MonoBehaviour
         else
         {
             curve[1].transform.position = transform.position;
-           
+            SetCurveHandles(lastDirection);
+
         }
-        
+
     }
+
+    //private void SetCurveHandles(Vector2 lastDirection)
+    //{
+    //    switch (lastDirection)
+    //    {
+    //        case Vector2(2, 0):
+
+    //            break;
+
+    //        case Vector2.down:
+    //            break;
+
+    //        case Vector2.left:
+    //            break;
+
+    //        case Vector2.right:
+    //            break;
+
+
+
+    //    }
+    //}
 
     public void Move(Vector3 movement)
     {
         transform.Translate(movement.normalized * speed * Time.deltaTime);
         lastDirection = movement;
     }
+
 }
