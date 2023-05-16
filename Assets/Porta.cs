08f5b0c6b1525537f3161409ba3199bb8e73292c
Bronom <federome@live.it>
2023-05-16 16:34:17 +0200

    modifica porta

--- Assets/Porta.cs
+++ Assets/Porta.cs
@@ -5,18 +5,26 @@ using UnityEngine;
 public class Porta : Interactable
 {
     [SerializeField] bool isOpen;
+    [SerializeField] bool canOpenWithKey;
+
 
     public override void Interact(Player player)
     {
-        if (player.stateMachine.GetCurrentState() is not PlayerStateBoomerangReturning && !isOpen)    //il giocatore deve essere umano o boomerang che cammina
-        {
+        if (player.stateMachine.GetCurrentState() is not PlayerStateBoomerangReturning && !isOpen)    //il giocatore deve essere umano o boomerang 
+        {                                                                                               
+            if (!canOpenWithKey)            //guarda se questa porta puo essere aperta da una chiave
+            {
+                Debug.Log("questa porta non puo essere aperta da una chiave");
+                return;
+            }
+            
             Inventory inventory = player.GetComponent<Inventory>();
 
             if (inventory != null)
             {
                 if (inventory.UseKey())
                 {
-                    isOpen = true;
+                    AbilitateDoor();
                     OpenDoor();
                 }
 
@@ -34,8 +42,13 @@ public class Porta : Interactable
         {
             //apro la porta
             OpenDoor();
-            
+
         }
+    }
+
+    public void AbilitateDoor()         //da usare su un evento se non puo essere aperto da una chiave;
+    {
+        isOpen = true;
     }
 
     private void OpenDoor()
