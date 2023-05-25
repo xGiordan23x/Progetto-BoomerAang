55869e8b8f4f63ef2a394097fe1bf645b8ec354e
Bronom <federome@live.it>
2023-05-25 16:41:42 +0200

    Fatto menu Pausa
    manca il salvataggi e opzioni

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void ContinueGame()
+    {
+        //quando faro i salvataggi     .fede
+    }
+
     public void QuitGame()
     {
         Application.Quit();
     }
+
+    
 }
---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    [SerializeField] GameObject pauseMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Save()
+    {
+        //quando si potrà salvare     .Fede
+        Debug.Log("Ho salvato");
+    }
+
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
