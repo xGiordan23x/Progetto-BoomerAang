fcb5c742d419a3947c63a0542c48f8633d65064f
Giordano23 <francigio23@gmail.com>
2023-06-01 17:59:28 +0200

    fatto altro riguardare porta

--- Assets/scripts/Utility/DinamicOrdering.cs
+++ Assets/scripts/Utility/DinamicOrdering.cs
@@ -2,24 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DinamicOrdering : MonoBehaviour
 {
     [SerializeField] SpriteRenderer renderer;
-
+   
     // Start is called before the first frame update
     void Start()
     {
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        ChangeLayer();
+       
+            ChangeLayer();
+        
+       
     }
 
+  
     private void ChangeLayer()
     {
         renderer.sortingOrder = (int)(-transform.position.y * 200);
@@ -27,7 +31,9 @@ public class DinamicOrdering : MonoBehaviour
 
     private void Awake()
     {
-        ChangeLayer();
+        renderer= GetComponent<SpriteRenderer>();
+       
+            ChangeLayer();
 
         if (gameObject.isStatic)
         {
