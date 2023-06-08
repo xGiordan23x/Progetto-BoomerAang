aca5b4d8e82057a2e51db2184ed400a6d185da6d
Fede <federome@live.it>
2023-06-08 18:03:45 +0200

    ggiocatore

--- Assets/Porta.cs
+++ Assets/Porta.cs
@@ -50,7 +50,8 @@ public class Porta : Interactable
                     OpenDoor();
 
                     //animazione
-                    
+                    player.animator.SetTrigger("OpenDoor");
+                    player.SetCanMove(0);
                 }
 
             }
