1d9d60b713ce9b163e19ffa3b4e23c90749be16f
Simone Bertè <smeraldsimone@gmail.com>
2023-06-15 12:23:17 +0200

    Aggiunta animazione Computer

---
+++ Assets/Scripts/DialogueSystem/Computer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Computer : Interactable
+{
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    [Header("Audio")]
+    [SerializeField] AudioClip ClipTurnOn;
+    [SerializeField] AudioClip ClipTurnOff;
+
+    public override void Interact(Player player)
+    {
+        if (player.stateMachine.GetCurrentState() is not PlayerStateBoomerangReturning)
+        {
+            animator.SetTrigger("On");
+
+            base.Interact(player);
+        }
+    }
+
+    public void PlayAudioClipTurnOn()
+    {
+        AudioManager.instance.PlayAduioClip(ClipTurnOn);
+    }
+
+    public void PlayAudioClipTurnOff()
+    {
+        AudioManager.instance.PlayAduioClip(ClipTurnOff);
+    }
+}
