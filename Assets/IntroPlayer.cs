baee03d5de50124317cec215011e4065f9d6d66f
xGiordan23x <francigio23@gmail.com>
2023-09-08 13:54:11 +0200

    temp

---
+++ Assets/IntroPlayer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class IntroPlayer : MonoBehaviour
+{
+    public PlayableDirector timelineIntro;
+    public GameObject blackScreen;
+    public GameObject introImage;
+    public AudioClip introMusicClip;
+
+
+    private void Start()
+    {
+        blackScreen.SetActive(false);
+        introImage.SetActive(false);
+    }
+
+    public void StartIntro()
+    {       
+        blackScreen.SetActive(true);
+        introImage.SetActive(true);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        timelineIntro.Play();
+
+    }
+    public void playIntroClip()
+    {        
+            AudioManager.instance.PlayAudioClip(introMusicClip);        
+    }
+}
--- Assets/scripts/Utility/MainMenu.cs
+++ Assets/scripts/Utility/MainMenu.cs
@@ -7,26 +7,8 @@ using UnityEngine.Timeline;
 
 public class MainMenu : MonoBehaviour
 {
-    public PlayableDirector timelineIntro;
-    public GameObject blackScreen;
-    public GameObject introImage;
-
-    private void Start()
-    {
-        blackScreen.SetActive(false);
-        introImage.SetActive(false);
-    }
-    public void StartGame()
-    {
-       // Play animazione o TimeLine
-     blackScreen.SetActive(true);
-     introImage.SetActive(true);
-     Cursor.visible = false;
-     Cursor.lockState = CursorLockMode.Locked;
-     timelineIntro.Play();
-
-    }
-
+    
+   
     public void ContinueGame()
     {
         GameManager.instance.LoadSavedRoom();
